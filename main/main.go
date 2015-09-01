25e4b585f0daea7187a8007159a7de2823bcaa3f
Abdul2 <Abdulrashid2@googlemail.com>
2015-09-01 10:36:19 +0100

    first commit

--- main/main.go
+++ main/main.go
@@ -24,14 +24,11 @@ func main() {
 		// Open will validate the arguments but will not create a connection
 		// to validate the source exist do a Ping 
 		//replace user 
-		db, err := sql.Open("postgres", "user=user-name dbname=phonebook host=localhost sslmode=disable")
+		db, err := sql.Open("postgres", "user=abdul-rashidabdi dbname=phonebook host=localhost sslmode=disable")
 
 		
 		//in case of error print the erro and exit 
-		if err != nil {
-		fmt.Printf(err.Error())
-		return
-		}
+		checkError(err)
 
 		// try a Ping
 		
@@ -48,13 +45,10 @@ func main() {
 		//when are you done with db oprations exit
 		defer db.Close()
 
-		//always antipate error
+		//always anticipate error
 		err = doSelect(db)
 	
-		if err != nil {
-		fmt.Printf("select error: %v\n",err)
-		return
-		}
+		checkError(err)
 
 }
 
@@ -67,20 +61,14 @@ func doSelect(db *sql.DB) error {
 	//use the db.Open() to return a prepared statement to be used at later stage 
 	stmt, err := db.Prepare(selectStatement)
 	
-	//if error, return it to main() to deal with the error
-	if err != nil {
-		fmt.Printf(err.Error())
-		return err
-	}
+	checkError(err)
 
 	//Query executes a prepared query statement with the given arguments
 	// and returns the query results as a *Rows. [this is direct from Go docs]
 
 	rows, err := stmt.Query()
-	if err != nil {
-		fmt.Printf(err.Error())
-		return err
-	}
+	
+	checkError(err)
  
  	//once done with statement close it.
 	defer stmt.Close()
@@ -92,10 +80,6 @@ func doSelect(db *sql.DB) error {
 		
 		// to avoid caller having copies of values, pointers are used 
 		err = rows.Scan(&id, &name)
-		if err != nil {
-			fmt.Printf("rows.Scan error: %v\n",err)
-			return err
-		}
 
 		// first %v is replaced by value of id and second %v by value of name 
 		fmt.Printf("firstname: %v lastname: %v \n", id, name);
@@ -104,6 +88,13 @@ func doSelect(db *sql.DB) error {
 	return nil
 }
 
+func checkError(err error){
+	
+		if err != nil {
+			panic(err)
+		}
+	
+}
 
 
 
